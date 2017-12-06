5947936523ff58a6093beb2cdcd07978323ab181
Xavi Aznar <xavi.aznar@outlook.com>
2017-12-06 21:19:55 +0100

    DNSMASQ 2.76-r5 en Alpine Linux 3.6 en Docker
    Fichero Dockerfile para construir la imagen con dnsmasq
    y los ficheros de configuracion para ejecutar el contenedor.
    
    Tambien se incluye el script `start-dns-sh` como ayuda para
    lanzar el contenedor con los parametros correctos.

---
+++ start-dns.sh
@@ -0,0 +1,9 @@
+CONTAINER_NAME="dns"
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker run -d --name $CONTAINER_NAME \
+    -p 53:53 -p 53:53/udp \
+    --cap-add=NET_ADMIN \
+    -v $(pwd)/dnsmasq.conf:/etc/dnsmasq.conf:ro \
+    -v $(pwd)/hosts:/etc/hosts:ro \
+xaviaznar/alpine-dnsmasq:3.6-2.76-r5
