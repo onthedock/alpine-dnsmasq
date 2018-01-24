a80b21e537240b7f9b9eb358bff8e634f8bbc31b
Xavi Aznar <xavi.aznar@outlook.com>
2018-01-24 21:03:18 +0100

    Actualizado el script de lanzamiento.

--- start-dns.sh
+++ start-dns.sh
@@ -6,4 +6,4 @@ docker run -d --name $CONTAINER_NAME \
     --cap-add=NET_ADMIN \
     -v $(pwd)/dnsmasq.conf:/etc/dnsmasq.conf:ro \
     -v $(pwd)/hosts:/etc/hosts:ro \
-xaviaznar/alpine-dnsmasq:3.6-2.76-r5
+xaviaznar/alpine-dnsmasq:3.7-2.78-r1
